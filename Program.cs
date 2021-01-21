4c04636302d16f313d6219636b7284e1a2ddb5a1
Microsoft <Microsoft@DESKTOP-7NSCBFQ>
2021-01-21 14:41:12 +0300

    1234

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Programlamaya başlangıç için temel kurs";
+            string kurs3 = "Java";
+             
+            //array - dizi
+
+            string[] kurslar = new string[] {"Yazılım Geliştirici Yetiştirme Kampı",
+                                            "Programlamaya başlangıç için temel kurs",
+                                            "Java",
+                                            "c#"};
+
+            //javada bu görüntü aynısı
+            //i=i+2 2 şer arttırır
+            //i < 3 yazarsan hala dinamik değilsindir.Dinamik olmak için dizinin boyutu kadar yaz dememiz lazım onuda kurslar.length şeklinde yaparız.
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            Console.WriteLine("for bitti");
+
+            //kursları tek tek dolaş
+            //foreach dizilere uygulanır
+            //kurs kısaltılmış isim gibi düşün
+            //strin kursları alıyor
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("sayfa sonu - footer");
+        }
+    }
+}
